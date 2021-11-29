ba1c9ce11ccce3ac0380682965d0ab7bc4a10d5f
Martín Javier Larré <75001420+Martin-J-Larre@users.noreply.github.com>
2021-11-29 05:27:37 -0500

    primera entrega

---
+++ routes/productos.js
@@ -0,0 +1,88 @@
+const { Router } = require("express");
+const contenedor = require("../contenedor");
+const moment = require('moment');
+const router = Router();
+
+const productos = new contenedor("productos.txt");
+const admin = true;
+
+function serverProductsRoutes(app) {
+    app.use("/api/productos", router);
+    // this.ok
+    router.get('/:id?', (req, res) => {
+        const {id} = req.params;
+        if (id) {
+            const object = productos.getById(id);
+            object.then(response => {
+                if (response) {
+                    res.json({
+                        admin,
+                        product: response
+                    });
+                } else {
+                    res.send("Producto no encontrado");
+                }
+            }).catch(err => console.log("Error en router.get()", err));
+        } else {
+            const allProducts = productos.getAll();
+            allProducts.then(response => {
+                res.json({
+                    admin,
+                    products: response
+                });
+            }).catch(err => console.log("Error en router.get()", err));
+
+        }
+    })
+
+    // Mistake here
+    // Trato de ingresar por postman por body pero solo me imprime "timestamp y el id del método"
+    
+    router.post('/', (req, res) => {
+        if (admin) {
+            const timestamp = moment().format('DD/MM/YYYY hh:mm:ss a');
+            const product = {timestamp, ...req.body}; //Look up the problem here
+            const savedProduct = productos.save(product);
+            savedProduct.then(response => res.json(response)).catch(err => console.log("Error en router.post()", err));
+        } else {
+            res.send("Solo disponible para administradores");
+        }
+    })
+
+    // Mismo problema acá no me deja ingresar el body por postman 
+    router.put('/:id', (req, res) => {
+        if (admin) {
+            const {id} = req.params;
+            const product = req.body;
+            const updatedProduct = productos.updateById(id, product);
+            updatedProduct.then(response => {
+                if (response) {
+                    res.json(response);
+                } else {
+                    res.send("Producto no encontrado");
+                }
+            }).catch(err => console.log("Error en router.put()", err));
+        } else {
+            res.send("Solo disponible para administradores");
+        }
+    })
+
+    //this ok
+    router.delete('/:id', (req, res) => {
+        if (admin) {
+            const {id} = req.params;
+            const deleteProduct = productos.deleteById(id);
+            deleteProduct.then(response => {
+                if (response) {
+                    res.json(response);
+                } else {
+                    res.send("Producto no encontrado");
+                }
+            }).catch(err => console.log("Error en router.get()", err));
+        } else {
+            res.send("Solo disponible para administradores");
+        }
+    })
+}
+
+module.exports = serverProductsRoutes;
